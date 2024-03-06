11f9be16f9562dda6048ff4c561b86e38d0afde6
style77 <stylek777@gmail.com>
2024-03-06 15:44:07 +0100

    refactor: remove ollama

--- app/main.py
+++ app/main.py
@@ -3,22 +3,15 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from contextlib import asynccontextmanager
 
-from ollama import AsyncClient
-
 from app.core.api import router as core_router
 from app.chat.api import router as chat_router
 from app.core.logs import logger
 from app.config import settings
 
 
-def init_client():
-    return AsyncClient(host=settings.OLLAMA_HOST)
-
-
 @asynccontextmanager
 async def lifespan(app: str):
     logger.info("Starting up the server")
-    app.state.client = init_client()
     yield
     logger.info("Shutting down the server")
 
