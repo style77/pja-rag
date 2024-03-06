3ff99f02ce40304978f38603a43527b6e3aa64c8
style77 <stylek777@gmail.com>
2024-03-06 15:46:12 +0100

    chore: change client title

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ChatContainer from "./components/ChatContainer";
 
 function App(): JSX.Element {
+  useEffect(() => {
+    document.title = "PJA-RAG"
+  }, [])
   return (
     <div className="App">
       <ChatContainer />
