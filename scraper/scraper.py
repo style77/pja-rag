1c531842f03e9dfc7dc9788726e7515b88f10655
style77 <stylek777@gmail.com>
2024-03-07 19:44:02 +0100

    chore: add url hashing to file name

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,5 +1,7 @@
 import asyncio
 from dataclasses import dataclass, field
+import hashlib
+import json
 import logging
 import os
 from typing import List, Optional
@@ -37,20 +39,39 @@ class Scraper:
 
         self.data = Directory(url=config.URL, name=config.URL)
 
+    def update_url_hash_mapping(self, url_hash: str, original_url: str):
+        mapping_file = 'url_hash_mapping.json'
+        try:
+            with open(mapping_file, 'r') as f:
+                mapping = json.load(f)
+        except FileNotFoundError:
+            mapping = {}
+
+        mapping[url_hash] = original_url
+
+        with open(mapping_file, 'w') as f:
+            json.dump(mapping, f, indent=4)
+
     async def download_file(self, file: File, directory_path: str):
+        url_hash = hashlib.md5(file.url.encode('utf-8')).hexdigest()[:10]
         async with aiohttp.ClientSession() as session:
             async with session.get(file.url) as response:
                 if response.status == 200:
-                    # Ensure the directory exists
                     os.makedirs(directory_path, exist_ok=True)
 
-                    file_path = os.path.join(directory_path, file.name)
+                    file_splitted = file.name.split('.')
+                    file_ext = file_splitted[-1]
+                    file_name = file_splitted[0]
+
+                    file_name_with_hash = f"{file_name}.{file_ext}_{url_hash}"
+                    file_path = os.path.join(directory_path, file_name_with_hash)
                     with open(file_path, 'wb') as f:
                         while True:
                             chunk = await response.content.read(1024)
                             if not chunk:
                                 break
                             f.write(chunk)
+                    self.update_url_hash_mapping(url_hash, file.url)
                     logging.info(f"Downloaded {file.url} to {file_path}")
                 else:
                     logging.error(f"Failed to download {file.url}: Status {response.status}")
@@ -66,7 +87,6 @@ class Scraper:
     async def fetch_directory_contents(self, driver, directory: Directory):
         links = await self._get_all_links(driver, directory.url)
         logging.info(f"Found {len(links)} links in {directory.url}")
-        logging.info(f"Links: {links}")
         for link in links:
             if self.is_directory(link):
                 subdirectory = Directory(url=link, name=link.split('/')[-1], parent=directory)
@@ -83,6 +103,7 @@ class Scraper:
 
     async def run(self):
         options = webdriver.ChromeOptions()
+        options.headless = True
         async with webdriver.Chrome(options=options) as driver:
             await self.fetch_directory_contents(driver, self.data)
 
